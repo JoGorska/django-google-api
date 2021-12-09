c98dbbcbd4de337ea82933bb56b164bb850ae4a5
JoGorska <83362036+JoGorska@users.noreply.github.com>
2021-12-09 22:21:33 +0000

    Initial commit

--- env.py
+++
@@ -1,9 +0,0 @@
-import os
-
-os.environ["DATABASE_URL"] = 'postgres://ziatavupscqmfn:eff050ed0e6d21876e575d559bf2bef184e5e4dad91bbe5fecba451b80c0bc4e@ec2-34-255-134-200.eu-west-1.compute.amazonaws.com:5432/db8lebclu26t13'
-
-os.environ['SECRET_KEY'] = 'django-insecure-0_d%heun&d8m34j!0fs9r#ut#($%4$$apzjac!vhc%l_wph_$m'
-
-os.environ['RECAPTCHA_SITE_KEY'] = '6LflW4sdAAAAABN8veB240edbYc4Ycr0Bq5CU7Io'
-os.environ['RECAPTCHA_SECRET_KEY'] = '6LflW4sdAAAAALFjIC8_RgMaHu6Rhm3ttCz3DIu0'
-os.environ['GOOGLE_API_KEY'] = 'AIzaSyAeIeU3QmWF_A1cVUmlS55o0FVr9n3KKho'
